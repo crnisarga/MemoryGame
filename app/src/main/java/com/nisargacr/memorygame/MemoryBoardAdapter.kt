85f01e7a7410cbba6529206cd32a07264be6ed9f
crnisarga <nisargagowdacr@gmail.com>
2024-09-04 13:52:14 +0530

    Memory Game recycler view adopted

--- app/src/main/java/com/nisargacr/memorygame/MemoryBoardAdapter.kt
+++ app/src/main/java/com/nisargacr/memorygame/MemoryBoardAdapter.kt
@@ -1,17 +1,32 @@
 package com.nisargacr.memorygame
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+        companion object {
+            private const val MarginSize = 10
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-       val view =  LayoutInflater.from(context).inflate(R.layout.memory_card,parent,false)
+        val cardWidth = parent.width / 2 - (2 * MarginSize)
+        val cardHeight = parent.height / 4 - (2 * MarginSize)
+        val cardSideLength = min(cardHeight,cardWidth)
+        val view =  LayoutInflater.from(context).inflate(R.layout.memory_card,parent,false)
+        val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as MarginLayoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        layoutParams.setMargins(MarginSize, MarginSize, MarginSize, MarginSize)
         return ViewHolder(view)
     }
 
@@ -22,8 +37,12 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private  val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
         fun bind(position: Int) {
-            //no ops
+            imageButton.setOnClickListener {
+                Log.d("myWork","the image clicked is in position $position")
+            }
         }
     }
 }
